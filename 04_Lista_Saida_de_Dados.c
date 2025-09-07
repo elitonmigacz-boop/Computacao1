99126023ccebc7dd78a8c21157b4ae94f2542d81
elitonmigacz-boop <elitonmigacz@gmail.com>
2025-09-07 14:24:45 -0300

    04_Lista_Saida_de_Dados.c

---
+++ 04_Lista_Saida_de_Dados.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int codigoA = 65;
+
+	printf("Letra: %c  Codigo ASCII: %d\n", codigoA, codigoA);
+	printf("Letra: %c  Codigo ASCII: %d\n", codigoA + 1, codigoA + 1);
+	printf("Letra: %c  Codigo ASCII: %d\n", codigoA + 2, codigoA + 2);
+	printf("Letra: %c  Codigo ASCII: %d\n", codigoA + 3, codigoA + 3);
+
+	return 0;
+}
