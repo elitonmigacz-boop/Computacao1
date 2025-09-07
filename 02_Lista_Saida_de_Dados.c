09684faea972628d939a7dbddc2356c13f8e5ce5
elitonmigacz-boop <elitonmigacz@gmail.com>
2025-09-07 13:41:41 -0300

    02_Lista_Saida_de_Dados.c

--- 02_Lista_Saida_de_Dados.c
+++ 02_Lista_Saida_de_Dados.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
-int main()
+int main ()
 {
-	printf ("Oi! Esse eh um programa teste!\n");
-	printf ("  *\n");
-	printf (" ***\n");
-	printf ("*****\n");
-	return 0;
-}
+ printf (" Esse eh um programa de saida de dados !!!\n");
+ printf ("A seguir alguns exemplos com a funcao printf :\n");
+ printf ("%d\n", 65);
+ printf ("%c\n", 65);
+ printf ("%d\n", 65) ;
+ printf ("%c\n", 'A' );
+ printf ("%d\n", 'A' );
+ printf ("%d\n", 10);
+ printf ("\n\n \n");
+ printf (" Fim do Programa !!! ");
+ 
+ return 0;
+ }
