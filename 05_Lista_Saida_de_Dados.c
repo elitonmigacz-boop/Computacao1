62c6a38172f4639573dea185176cfd73b6ee09f5
elitonmigacz-boop <elitonmigacz@gmail.com>
2025-09-07 14:28:52 -0300

    05_Lista_Saida_de_Dados.c

---
+++ 05_Lista_Saida_de_Dados.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("Letra minuscula: %c\n", 65 + 32);
+	printf("Letra minuscula: %c\n", 66 + 32);
+	printf("Letra minuscula: %c\n", 67 + 32);
+	printf("Letra minuscula: %c\n", 68 + 32);
+	return 0;
+}
